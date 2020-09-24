ebfca223f54f52d03ffe2ad28ecbfd7cc3dc20ed
unknown <sijm@naver.com>
2020-09-25 00:29:57 +0900

    firabase db get

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,13 @@ const Home = () => {
     const [nweets,setNweets] = useState([]);
     const getNweets = async () => {
         const dbNweets = await dbService.collection('nweets').get();
-        dbNweets.forEach((document) => console.log(document.data()))
+        dbNweets.forEach((document) => {
+            const nweetObject ={
+               ...document.data(),
+                id:document.id,
+            }
+            setNweets( (prev) => [nweetObject, ...prev] );
+        });
     };
     useEffect (() =>{
         getNweets();
@@ -26,12 +32,20 @@ const Home = () => {
             ,} = event;
             setNweet(value);
     };
+    console.log(nweets);
     return(
         <div>
             <form onSubmit={onSubmit}>
                 <input onChange={onChange}  value={nweet} type='text' placeholder="what's on your mind" maxLength={120} /> 
                 <input type='submit' value="Nweet" />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
